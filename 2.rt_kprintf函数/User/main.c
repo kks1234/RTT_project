cd58784637f8e2f01cb48d3e7a8f750ad575af47
kks1234 <945366712@qq.com>
2019-04-10 20:44:29 +0800

    rt_kprintf

---
+++ 2.rt_kprintf函数/User/main.c
@@ -0,0 +1,66 @@
+#include "board.h"
+#include "rtthread.h"
+
+
+ALIGN(RT_ALIGN_SIZE)
+
+static rt_uint8_t stack_led[1024];
+
+struct rt_thread led1;
+
+void led_entry(void*parameter)
+{
+	while(1)
+	{
+		rt_thread_delay(500);
+		GPIO_SetBits(GPIOB, GPIO_Pin_5);
+		rt_kprintf("led1 off\n");
+		rt_thread_delay(500);
+		GPIO_ResetBits(GPIOB, GPIO_Pin_5);
+		rt_kprintf("led1 on\n");
+		
+	}
+	
+}
+
+
+ALIGN(RT_ALIGN_SIZE)
+
+rt_thread_t led2;
+
+void led2_entry(void*parameter)
+{
+	while(1)
+	{
+		rt_thread_delay(500);
+		GPIO_SetBits(GPIOE, GPIO_Pin_5);
+		rt_kprintf("led2 off\n");
+		rt_thread_delay(500);
+		GPIO_ResetBits(GPIOE, GPIO_Pin_5);
+		rt_kprintf("led2 on\n");
+		
+	}
+	
+}
+
+
+int main()
+{
+	rt_thread_init(&led1,"led",led_entry,RT_NULL,stack_led,sizeof(stack_led),3,20);
+	
+	rt_thread_startup(&led1);
+	
+	led2 = rt_thread_create("led2",led2_entry,RT_NULL,500,4,20);
+	
+	if(led2 != RT_NULL)
+		rt_thread_startup(led2);
+	else
+		return -1;
+	
+	
+}
+
+
+
+
+
